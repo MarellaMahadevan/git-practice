4b028c50de91b0e349372e72230ccf1fe0b72d3a
MarellaMahadevan <marellamahadevan009@gmail.com>
2024-08-26 16:48:33 +0530

    commit all files from stage area

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+$PERSON1=Ramesh
+$PERSON2=Suresh
+
+echo "$PERSON1:: Hi suresh,How are you?"
+echo "Suresh:: I' am fine ,How are you doing?"
+echo "$PERSON1:: I am doing good ${PERSON1} ,what's going on?"
+echo "$PERSON1:: I am learning shell scripting"
