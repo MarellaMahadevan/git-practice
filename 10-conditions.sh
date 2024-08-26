73824a2f095323df55658d41ac8794ab3dd3d493
MarellaMahadevan <marellamahadevan009@gmail.com>
2024-08-26 20:47:10 +0530

    commit all files from stage area

--- 10-conditions.sh
+++ 10-conditions.sh
@@ -2,9 +2,10 @@
 
 NUMBER=$1
 
-if[ $NUMBER -gt 20 ] #-gt ,lt,eq,-ne
+
+if [ $NUMBER -gt 20 ]
 then
-    echo "Given Number : $NUMBER is greater than 20"
+   echo "Given Number is $NUMBER is greater than 20"
 else
-    echo "Given Number :$NUMBER is less than 20"
+   echo "Given Number is $NUMBER is less than 20"
 fi
