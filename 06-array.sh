065cc2c9110ec46aca10f82c3723e10e14f80e3f
MarellaMahadevan <marellamahadevan009@gmail.com>
2024-08-26 17:37:03 +0530

    commit all files from stage area

--- 06-array.sh
+++ 06-array.sh
@@ -2,9 +2,7 @@
 
 FRUITS=("MANGO" "KIWI" "ORANGE")
 
-echo "first fruit is: ${FRUITS[0]}"
-echo "second fruit is: ${FRUITS[1]}"
-echo "first fruit is: ${FRUITS[2]}"
+
 echo "all fruit is: ${FRUITS[@]}"
 
 
