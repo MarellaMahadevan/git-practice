5d9e4eb7f81af8afd9e3583131b18268f1d83d32
MarellaMahadevan <marellamahadevan009@gmail.com>
2024-08-29 20:06:37 +0530

    all file should be committed

--- 06-array.sh
+++ 06-array.sh
@@ -2,4 +2,8 @@
 
 FRUITS=("APPLE" "BANANA" "ORANGE" "CHIPS")
 
-echo "All Fruits ${FRUITS[0]}"
+echo "First Fruit ${FRUITS[0]}"
+echo "Second Fruit ${FRUITS[1]}"
+echo "Third Fruit ${FRUITS[2]}"
+
+echo "All Fruits ${FRUITS[@]}"
