9f5e8df763b8b0e2c9f41aadbb78cf7ea0331046
MarellaMahadevan <marellamahadevan009@gmail.com>
2024-08-29 16:59:38 +0530

    files to be committed

--- 05-variables.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-PERSON1=$1
-PERSON2=$2
-
-echo "$PERSON1:: Hi suresh,How are you?"
-echo "Suresh:: I' am fine ,How are you doing?"
-echo "$PERSON1:: I am doing good ${PERSON1} ,what's going on?"
-echo "$PERSON1:: I am learning shell scripting"
