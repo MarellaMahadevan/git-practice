126e98167c88f2a4ea70fb2ea3480492bc779e99
MarellaMahadevan <marellamahadevan009@gmail.com>
2024-08-29 16:43:22 +0530

    Conversations between Ramesh and Suresh

--- 02-conversations.sh
+++ 02-conversations.sh
@@ -3,4 +3,4 @@
 echo "Ramesh :: Hi Suresh How are you?"
 echo "Suresh :: Hi Ramesh I am fine ,How are you doing"
 echo "Ramesh :: I am doing good Ramesh ,What's goin on "
-echo "Suresh :: I started shell scripting Ramesh
+echo "Suresh :: I started shell scripting Ramesh"
