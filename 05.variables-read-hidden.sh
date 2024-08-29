ed916ad4a6ece0fbf4a9e3ba3aebb08f3fcb34a8
MarellaMahadevan <marellamahadevan009@gmail.com>
2024-08-29 17:05:02 +0530

    files to be committed

---
+++ 05.variables-read-hidden.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Please enter username"
+
+read -s USERNAME 
+
+echo "UserName is $USERNAME"
+
+echo "Please enter user password"
+
+read -s PASSWORD
+
+echo "User Password is $PASSWORD "
