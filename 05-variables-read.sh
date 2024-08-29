60a3d0096f99dbc7f3b967eb58cf340fea5db012
MarellaMahadevan <marellamahadevan009@gmail.com>
2024-08-29 19:58:38 +0530

    Read the data from terminal

--- 05-variables-read.sh
+++ 05-variables-read.sh
@@ -4,6 +4,6 @@ echo "Please Enter User Name:"
 read USERNAME
 echo "UserName is :- $USERNAME"
 
-echo "Please Enter User Password
+echo "Please Enter User Password"
 read PASSWORD
 echo "UserPassword is :- $PASSWORD"
