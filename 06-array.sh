1242ab0f2c2e384a67f0a1eb6b50cbf51de44b2a
MarellaMahadevan <marellamahadevan009@gmail.com>
2024-08-29 17:13:59 +0530

    files to be committed

--- 06-array.sh
+++ 06-array.sh
@@ -3,8 +3,8 @@
 
 FRUITS=("APPLE" "KIWI" "ORANGE")
 
-echo "First fruit is $FRUITS[0]"
-echo "Second fruit is $FRUITS[1]"
+echo "First fruit is ${FRUITS[0]}"
+echo "Second fruit is ${FRUITS[1]}"
 echo "Third fruit is ${FRUITS[2]}"
 
 echo "All Fruits is ${FRUITS[@]}"
