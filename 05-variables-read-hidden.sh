c92134d7aa994f598aa9c40e762726b65bc3e9f2
MarellaMahadevan <marellamahadevan009@gmail.com>
2024-08-29 19:48:04 +0530

    Pass the data from terminal

--- 05-variables-read-hidden.sh
+++ 05-variables-read-hidden.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-echo "Please Enter User Name:-"
-
-read USERNAME
+echo "Please Enter User Name:-" read USERNAME
 
 echo "UserName is $USERNAME"
+
+echo "Please Enter Password:-" read PASSWORD
+
+echo "PassWord is $PASSWORD"
