2cd1c99a18ac9c4b2c1a82bba6f50f3c39d3cc50
MarellaMahadevan <marellamahadevan009@gmail.com>
2024-08-29 16:13:43 +0530

    Welcome to the Shell Scripting

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-#ctrl + s ---> save this data it is not part of your code
-# Author ---> Marella Mahadevan
-# Course ---> Devops With AWS
-echo "Hello World!"
+echo "Welcome to the Shell Scripting"
