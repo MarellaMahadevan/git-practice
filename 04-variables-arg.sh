aa073827df806c13218ba9a2c2ad2caa6b898e82
MarellaMahadevan <marellamahadevan009@gmail.com>
2024-08-29 19:39:22 +0530

    Arguments passed from terminal

--- 04-variables-arg.sh
+++ 04-variables-arg.sh
@@ -2,8 +2,15 @@
 
 PERSON1=$1
 PERSON2=$2
+PERSON3=$3
+PERSON4=$4
 
 echo "$PERSON1 :: Hi $PERSON2 How are you?"
 echo "$PERSON2 :: Hi $PERSON1 I am fine ,How are you doing"
 echo "$PERSON1 :: I am doing good $PERSON1 ,What's goin on "
 echo "$PERSON2 :: I started shell scripting $PERSON1"
+
+echo "$PERSON3 :: Hi $PERSON4 How are you?"
+echo "$PERSON4 :: Hi $PERSON3 I am fine ,How are you doing"
+echo "$PERSON3 :: I am doing good $PERSON4 ,What's goin on "
+echo "$PERSON4 :: I started shell scripting $PERSON3"
