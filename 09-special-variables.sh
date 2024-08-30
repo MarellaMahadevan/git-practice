dd9b32c2d63bf55277c68a6000cca296f4d14f55
MarellaMahadevan <marellamahadevan009@gmail.com>
2024-08-30 15:56:58 +0530

    special-variables

--- 09-special-variables.sh
+++ 09-special-variables.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
 
-echo "All varibles passed to the script :- $@ "
-echo "Number of variables passed:- $#"
-echo "script name:- $0"
-echo "present working directory : $PWD"
-echo "Current Home directory :- $HOME"
-echo "processing id of the current script $$"
-
-sleep 100 &
-
-echo "PID of the background command $!"
+echo "All Variables should be passed to script : $@"
