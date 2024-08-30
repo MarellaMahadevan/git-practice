a4206c921b3604fc8f105543dbe9040aac208dd0
MarellaMahadevan <marellamahadevan009@gmail.com>
2024-08-30 16:27:03 +0530

    special-variables

--- 10-conditions.sh
+++ 10-conditions.sh
@@ -4,7 +4,7 @@ NUMBER=$1
 
 if [ $NUMBER -gt 20 ] #gt lt eq -ne -le -ge
 then
-   echo "Given Number is $NUMBER greater than 20"
+   echo "Given Number $NUMBER is greater than 20"
 else 
-   echo "Given Number is $NUMBER less than 20"
+   echo "Given Number  $NUMBER is less than 20"
 fi
