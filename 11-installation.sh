4c2434fc25c7232f15aa64b49257b9528aa62b12
MarellaMahadevan <marellamahadevan009@gmail.com>
2024-08-30 16:51:50 +0530

    special-variables

--- 11-installation.sh
+++ 11-installation.sh
@@ -2,5 +2,11 @@
 
 
 USERID=$(id -u)
+#echo "UserID is : $USERID"
 
-echo "UserID is : $USERID"
+if [ $USERID -ne 0 ]
+then
+    echo"Please run this script with root Previliges"
+fi
+
+dnf install git -y
