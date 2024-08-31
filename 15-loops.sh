7303c28629b511b6c5e3beab38df24035cee7673
MarellaMahadevan <marellamahadevan009@gmail.com>
2024-08-31 17:20:05 +0530

    shell

--- 15-loops.sh
+++ 15-loops.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 USERID=$(id -u)
+#echo "UserID is : $USERID"
+
 R="\e[31m"
 G="\e[32m"
 N="\e[0m"
@@ -8,33 +10,23 @@ N="\e[0m"
 CHECK_ROOT(){
     if [ $USERID -ne 0 ]
     then
-        echo "Please run this script with root priveleges"
+        echo "Please run this script with root Previliges"
         exit 1
     fi
 }
 
 VALIDATE(){
-    if [ $1 -ne 0 ]
-    then
-        echo -e "$2 is...$R FAILED $N"
-        exit 1
-    else
-        echo -e "$2 is... $G SUCCESS $N"
+    if [ $? -ne 0 ]
+    then 
+        echo -e "$2 is  $R Failed.. $N"
+    else 
+        echo -e "$2 is $G Success.. $N"
     fi
 }
 
 CHECK_ROOT
 
-# sh 15-loops.sh git mysql postfix nginx
-for package in $@ # $@ refers to all arguments passed to it
-do
-    dnf list installed $package
-    if [ $? -ne 0 ]
-    then
-        echo "$package is not installed, going to install it.."
-        dnf install $package -y
-        VALIDATE $? "Installing $package"
-    else
-        echo "$package is already installed..nothing to do"
-    fi
+for package in $@  #refers to all arguments  passed to it
+do 
+
 done
